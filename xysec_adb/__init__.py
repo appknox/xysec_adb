bfcbeb8862b0612aae1befa7755e78916461198a
dhilipsiva <dhilipsiva@gmail.com>
2015-03-11 07:43:05 +0530

    Minor file re-arrangement

---
+++ xysec_adb/__init__.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+# Copyright © Appknox
+#
+
+'''
+    File name: __init__.py
+    Version: 0.1
+    Author: dhilipsiva <dhilipsiva@gmail.com>
+    Date created: 2015-03-11
+'''
+__author__ = "dhilipsiva"
+__status__ = "development"
+
+"""
+
+"""
